4fe5e3b541a091e933fe2cd125124a339d35699c
Gavrilova_Tanya <tanya.belova19@yandex.ru>
2021-08-30 23:33:00 +0300

    Работа с условиями и массивами. App part 2

--- src/script.js
+++ src/script.js
@@ -1,10 +1,17 @@
 "use strict";
 
-//Task#1
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-console.log(numberOfFilms);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// for (let i=0; ; i++){
+//     let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+//     console.log(numberOfFilms);
+//     if(numberOfFilms.length !== 0 && numberOfFilms !== null
+//     && numberOfFilms !== undefined && numberOfFilms.length < 50 ){
+//        console.log('ok');
+//         break;
+//     } 
+// }
  
-//Task#2
 const personalMovieDB = {
 count: numberOfFilms,
 movies:{},
@@ -13,14 +20,22 @@ genres:[],
 privat: false
 };
 
-//Task3
-const filmName1 = prompt("Один из последних просмотренных фильмов?", ""),
-      filmEstimate1 = prompt("На сколько оцените его?", ""),
-      filmName2 = prompt("Один из последних просмотренных фильмов?", ""),
-      filmEstimate2 = prompt("На сколько оцените его?", "");
-
-
-personalMovieDB.movies[filmName1] = filmEstimate1;
-personalMovieDB.movies[filmName2] = filmEstimate2;
+for (let i=0; i <2; ){
+    let filmName = prompt("Один из последних просмотренных фильмов?", ""),
+        filmEstimate = prompt("На сколько оцените его?", "");
+    if( filmName !== null &&  filmEstimate !== null && filmName !== "" && filmEstimate !== ""
+    && filmName.length < 50 && filmEstimate.length < 50){
+        personalMovieDB.movies[filmName] = filmEstimate;
+        i++;
+    }
+}
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10){
+    console.log("просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы - классический зритель");
+}else if (personalMovieDB.count >= 30){
+    console.log("Вы - киноман");
+} else console.log("Произошла ошибка");
